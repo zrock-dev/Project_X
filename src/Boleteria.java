79773402bc20768b4099088461c8e72a6792b586
TheLocoto <DiegoFigueSevi@gmail.com>
2022-03-16 16:44:26 -0400

    a boleto le agregue el tipo de pelicula cambios en la linea 43

--- src/Boleteria.java
+++ src/Boleteria.java
@@ -40,8 +40,8 @@ public class Boleteria {
 
     public void comprarBoleto(int ci, MetodoPago metodo, String columnaAsiento, int cantidadAsientos,
                               String codigoSala){
-        Boleto boleto = new Boleto(registroClientes.get(ci), precioPelicula, salaMap.get(codigoSala));
-        boleto.aplicarDescuento(metodo,Semana.DOMINGO,"3D");
+        Boleto boleto = new Boleto(registroClientes.get(ci), precioPelicula, salaMap.get(codigoSala),"2D");
+        boleto.aplicarDescuento(metodo,Semana.DOMINGO,boleto.tipoPelicula);
         boleto.comprarAsientos(columnaAsiento, cantidadAsientos);
     }
 
