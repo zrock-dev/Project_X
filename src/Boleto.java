7aad428c6720a37862fc0fa9410b0d0c4dd2f938
TheLocoto <101671511+TheLocoto@users.noreply.github.com>
2022-03-16 16:09:11 -0400

    Update Boleto.java
    1ra parte de la actualizacion de aplicarDescuento. 
    Igual edite un poco en boleteria

--- src/Boleto.java
+++ src/Boleto.java
@@ -6,29 +6,66 @@ public class Boleto {
     Sala salaAsignada;
     String generoPelicula;
 
-    public Boleto(Cliente cliente, int precioPelicula, Sala sala){
+    String tipoPelicula;
+
+    public Boleto(Cliente cliente, int precioPelicula, Sala sala, String tipoPelicula){
         this.cliente = cliente;
         this.precioPelicula = precioPelicula;
         this.salaAsignada = sala;
+
+        this.tipoPelicula=tipoPelicula;
     }
 
     public void setGeneroPelicula(String generoPelicula) {
         this.generoPelicula = generoPelicula;
     }
 
-    public void aplicarDescuento(Boleteria.MetodoPago metodo){
+    public void aplicarDescuento(Boleteria.MetodoPago metodo,Semana dia,String tipoPelicula){
         int edadCliente = cliente.getEdad();
-        if (edadCliente >= 60){
-            precioPelicula -= (precioPelicula * .5);
-            puntosCliente -= (puntosCliente *.5);
-        }else if (edadCliente <= 10 && generoPelicula.equals("Animacion")){
-            precioPelicula -= (precioPelicula * .15);
-            puntosCliente -= (puntosCliente *.5);
-        }
-        if (metodo == Boleteria.MetodoPago.TARJETA){
-            precioPelicula -= precioPelicula * .12;
+        switch(tipoPelicula){
+            case "2D":
+                if (metodo.equals(Boleteria.MetodoPago.TARJETA) && dia.equals(Semana.JUEVES)){
+                    precioPelicula -= 40 * .12;
+                    puntosCliente -= 40 * .12;
+                }
+                if (edadCliente <= 10 && generoPelicula.equals("Animacion")){
+                    precioPelicula -= (40 * .15);
+                    puntosCliente -= (40 *.15);
+                }
+                if (dia.equals(Semana.MIERCOLES)){
+                    precioPelicula -= (40*.5);
+                    puntosCliente -= (40*.5);
+                    break;
+                }
+                if (edadCliente >= 60){
+                    precioPelicula -= (40 * .5);
+                    puntosCliente -= (40 *.5);
+                    break;
+                }
+
+            case "3D":
+                if (metodo.equals(Boleteria.MetodoPago.TARJETA) && dia.equals(Semana.JUEVES)){
+                    precioPelicula -= 60 * .12;
+                    puntosCliente -= 60 * .12;
+                }
+                if (edadCliente <= 10 && generoPelicula.equals("Animacion")){
+                    precioPelicula -= (60 * .15);
+                    puntosCliente -= (60 *.15);
+                }
+                if (dia.equals(Semana.MIERCOLES)){
+                    precioPelicula -= (60*.5);
+                    puntosCliente -= (60*.5);
+                    break;
+                }
+                if (edadCliente >= 60){
+                    precioPelicula -= (60 * .5);
+                    puntosCliente -= (60 *.5);
+                    break;
+                }
+                
         }
         setPuntosCliente();
+        setPrecioPelicula(precioPelicula);
     }
 
     public void comprarAsientos(String columnaSala, int cantidadAsientos){
@@ -38,6 +75,9 @@ public class Boleto {
 
     public void setPuntosCliente(){
         cliente.sumarPuntos(puntosCliente);
+    }
+    public void setPrecioPelicula(int precio){
+        this.precioPelicula=precio;
     }
 
 }
