a57befb178e277bcb908755067f43d3fd08eddb7
Santiago <santisaavcon@gmail.com>
2022-03-24 11:02:02 -0400

    candyBar miniMenu implemented

--- src/ServicioAlCliente/CanjearPromo.java
+++ src/ServicioAlCliente/CanjearPromo.java
@@ -1,6 +1,7 @@
 package ServicioAlCliente;
-
 import Registro.Cliente;
+
+import static Console_interaction.Utils.sn;
 
 public class CanjearPromo {
 
@@ -37,13 +38,26 @@ public class CanjearPromo {
             }
         }
     }
-    /*public static Map<Integer, String>  listaPremios(){
-        Map<Integer, String> premios = new ;
-        premios.put(Premios.PRIMERAOPCION.getPuntosPremio(),Premios.PRIMERAOPCION.getDescripcion());
-        premios.put(Premios.SEGUNDAOPCION.getPuntosPremio(),Premios.SEGUNDAOPCION.getDescripcion());
-        premios.put(Premios.TERCERAOPCION.getPuntosPremio(),Premios.TERCERAOPCION.getDescripcion());
-        premios.put(Premios.CUARTAOPCION.getPuntosPremio(),Premios.CUARTAOPCION.getDescripcion());
-        premios.put(Premios.QUINTAOPCION.getPuntosPremio(),Premios.QUINTAOPCION.getDescripcion());
-        premios.put(Premios.SEXTAOPCION.getPuntosPremio(),Premios.SEXTAOPCION.getDescripcion());
-        return premios;*/
+
+    public void menu(Cliente cliente) {
+        System.out.println("Menu \n 1.-Pipocas \n 2.-Refresco \n 3.-nachos");
+        System.out.println("¿Que desea comprar? Marque 1 para Pipocas 2 para Refresco 3 para nachos");
+        int indice = Integer.parseInt(sn.nextLine());
+        switch (indice) {
+            case 1:
+                System.out.println("1.-Pequeño_____15Bs \n 2.-Mediano_____25Bs \n 3.-Grande_____35Bs");
+                System.out.println("¿Cual desea comprar? Marque 1 para Pequeño 2 para Mediano 3 para Grande");
+                int indice1 = Integer.parseInt(sn.nextLine());
+            case 2:
+                System.out.println("1.-Pequeño_____10Bs \n 2.-Mediano_____25Bs \n 3.-Grande_____20Bs");
+                System.out.println("¿Cual desea comprar? Marque 1 para Pequeño 2 para Mediano 3 para Grande");
+                int indice2 = Integer.parseInt(sn.nextLine());
+                System.out.println("Que sabor quiere: 1.-CocaCola \n 2.-Fanta \n 3.- Sprite");
+                int indiceA = Integer.parseInt(sn.nextLine());
+            case 3:
+                System.out.println("1.-Pequeño_____10Bs \n 2.-Mediano_____20Bs \n 3.-Grande_____30Bs");
+                System.out.println("¿Cual desea comprar? Marque 1 para Pequeño 2 para Mediano 3 para Grande");
+                int indice3 = Integer.parseInt(sn.nextLine());
+        }
+    }
 }
